9e8ac8365d18e57baf73e81ca36dbf3184057d2e
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-12-06 14:38:37 +0100

    Adding one-to-one relationship  MinimumAgeHandler

---
+++ JWTAuthDemo/Helpers/MinimumAgeHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace JWTAuthDemo.Helpers
+{
+    public class MinimumAgeHandler : AuthorizationHandler<MinimumAgeRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MinimumAgeRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == ClaimTypes.DateOfBirth &&
+                                       c.Issuer == "http://localhost:5232"))
+            {
+                return Task.CompletedTask;
+            }
+            var dateOfBirth = Convert.ToDateTime(context.User.FindFirst(c => c.Type == ClaimTypes.DateOfBirth &&
+                                                                        c.Issuer == "http://localhost:5232")?.Value);
+            int calculatedAge = DateTime.Today.Year - dateOfBirth.Year;
+            if (dateOfBirth > DateTime.Today.AddYears(-calculatedAge))
+            {
+                calculatedAge--;
+            }
+
+            if (calculatedAge >= requirement.MinimumAge)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
