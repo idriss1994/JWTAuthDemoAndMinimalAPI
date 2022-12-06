fa2fd977e05ad69042f0e63324b7fd2b0cd2dd23
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-12-06 14:37:21 +0100

    Adding Minimum age requirement

---
+++ JWTAuthDemo/Helpers/MinimumAgeRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace JWTAuthDemo.Helpers
+{
+    public class MinimumAgeRequirement : IAuthorizationRequirement
+    {
+        public MinimumAgeRequirement(int age)
+        {
+            MinimumAge = age;
+        }
+        public int MinimumAge { get; }
+    }
+}
