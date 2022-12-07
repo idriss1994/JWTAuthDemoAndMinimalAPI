2075ecc5c973558a4b4ea2f3331e313b949eb67e
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-12-07 01:40:39 +0100

    Adding TemporaryStickerHandler for BuildingEntryRequirement

---
+++ JWTAuthDemo/Helpers/TemporaryStickerHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace JWTAuthDemo.Helpers
+{
+    public class TemporaryStickerHandler : AuthorizationHandler<BuildingEntryRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, BuildingEntryRequirement requirement)
+        {
+            if (context.User.HasClaim(c => c.Type == "TemporaryBadgeId" && c.Issuer == "https://microsoftsecurit"))
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
