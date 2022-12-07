b6447e0a18a2917ca8ff5a45bef2f6dcfa749313
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-12-07 01:36:13 +0100

    Adding BadgeEntryHandler for BuildingEntryRequirement

---
+++ JWTAuthDemo/Helpers/BadgeEntryHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace JWTAuthDemo.Helpers
+{
+    public class BadgeEntryHandler : AuthorizationHandler<BuildingEntryRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, BuildingEntryRequirement requirement)
+        {
+            if (context.User.HasClaim(c => c.Type == "BadgeId" && c.Issuer == "https://microsoftsecurit"))
+            {
+                context.Succeed(requirement);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
