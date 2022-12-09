a6afe101089f8d78fc1db94e841915eaf7e2e243
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2022-12-09 14:32:37 +0100

    Adding constructors to TodoDTO model

--- MinimalWebAPI/Models/TodoDTO.cs
+++ MinimalWebAPI/Models/TodoDTO.cs
@@ -2,8 +2,17 @@
 {
     public class TodoDTO
     {
+        public TodoDTO()
+        {
+        }
+        public TodoDTO(Todo todoItem)
+        {
+            (Id, Name, IsComplete) = (todoItem.Id, todoItem.Name, todoItem.IsComplete);
+        }
         public int Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+
+        
     }
 }
